1434b8b27060619fc40da5b966ef60016bfdfc95
Nikhil Gupta <Gup7nik@gmail.com>
2023-05-17 14:55:34 +0530

    Update EmployeeForm.js

--- frontend/src/components/EmployeeForm.js
+++ frontend/src/components/EmployeeForm.js
@@ -39,7 +39,7 @@ const EmployeeForm = () => {
             </label>
             <div className="mt-2.5">
               <input
-                className="flex wwhite h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                className="flex  h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900 wwhite"
                 type="text"
                 placeholder="Enter You Full Name"
                 {...register("name")}
@@ -57,7 +57,7 @@ const EmployeeForm = () => {
             </label>
             <div className="mt-2.5">
               <input
-                className="flex wwhite h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                className="flex  h-10 w-full rounded-md border border-gray-300 bg-transparent py-2 px-3 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900 wwhite"
                 type="email"
                 placeholder="Enter Your Email"
                 {...register("email")}
