076078355d385ff3ce3cf0ea0419f88defbb5a16
Nikhil Gupta <Gup7nik@gmail.com>
2023-06-05 10:59:58 +0530

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { createUser } = require("../controller/createUser");
 const { getUser } = require("../controller/getUsers");
+const { updateUser } = require("../controller/updateUser");
+const { deleteAllUsers,deleteUser } = require("../controller/deleteAllUser");
 router.post("/createUser", createUser);
 router.get("/getallUsers", getUser);
+router.put("/updateUsers/:id",updateUser);   
+router.delete("/deleteAllUsers",deleteAllUsers);
+router.delete("/deleteUser/:id",deleteUser);
+
 
 module.exports = router;
